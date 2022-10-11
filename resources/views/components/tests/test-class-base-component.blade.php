25b6228888118c956667e7aa7ab532cc38913289
okuda1506 <okuda1506@gmail.com>
2022-10-11 14:48:40 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+</div>
